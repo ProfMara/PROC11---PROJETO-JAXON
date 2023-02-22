6102569d3c37c1fde7b23475c03a3339cfabce0b
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-02-22 14:45:48 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,48 +4,49 @@ var i;
 
 
 function preload(){
-pathImg = loadImage("path.png");
-boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+ pathImg = loadImage("path.png");
+ boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
 function setup(){
- createCanvas(400,400);
+  createCanvas(400,400);
 
-// Movendo o fundo 
-
-
-
-//Criando menino que corre 
-boy = createSprite(200,350);
-boy.addAnimation("correndo",boyImg);
-boy.scale =0.07;
-
-// Criando Boundary (Limite) esquerdo  
-leftBoundary=createSprite(0,0,100,800);
-leftBoundary.visible = false;
+ // Movendo o fundo 
 
 
-//Crie Boundary direito 
+
+ //Criando menino que corre 
+ boy = createSprite(200,350);
+ boy.addAnimation("correndo",boyImg);
+ boy.scale =0.07;
+
+ // Criando Boundary (Limite) esquerdo  
+ leftBoundary=createSprite(0,0,100,800);
+ leftBoundary.visible = false;
+
+
+ //Crie Boundary direito 
 
 }
 
 
 function draw() {
-background(0);
+ background(0);
 
 
-// Menino se movendo no eixo X com o mouse
-edges= createEdgeSprites();
-boy.collide(edges[3]);
+ // Menino se movendo no eixo X com o mouse
 
-
-
- //Reiniciar o fundo
- 
+ edges= createEdgeSprites();
+ boy.collide(edges[3]);
 
 
 
- drawSprites();
+  //Reiniciar o fundo
+
+
+
+
+  drawSprites();
 
  }
 
